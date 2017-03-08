69356f27bd77620c17b173dac086a71da25dfd53
Cedric <cednewein@live.fr>
2017-03-09 00:26:45 +0100

    Nouveaux changements sur app.js, ajout de thèmes et gestion nom d'utilisateur

--- app.js
+++ app.js
@@ -7,17 +7,71 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
 
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
 
-        res.writeHead(200, {"Content-Type": "text/html"});
+      fs.readFile('index.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
 
-        res.end(content);
+    }
 
-    });
+    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
+
+      fs.readFile(__dirname + '/js/script.js', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+
+      fs.readFile(__dirname + '/css/style.css', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+    if(req.url.indexOf('.png') != -1){ //req.url has the pathname, check if it conatins '.css'
+
+      fs.readFile(__dirname + '/img/battleships.png', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'binary/img'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+    if(req.url.indexOf('.woff') != -1){ //req.url has the pathname, check if it conatins '.css'
+
+      fs.readFile(__dirname + '/fonts/vermin_vibes.woff', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/font'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+    else{
+    	fs.readFile('index.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+    }
 
 });
 
-
+var bateaux =[];
+var tirs = [];
 // Chargement de socket.io
 
 var io = require('socket.io').listen(server);
@@ -26,19 +80,22 @@ var io = require('socket.io').listen(server);
 // Quand un client se connecte, on le note dans la console
 
 io.sockets.on('connection', function (socket) {
-
     console.log('Un client est connecté !');
     //socket.emit('message', 'Vous êtes bien connecté !');
-    socket.on('message', function (message) {
-
-        console.log('Un client me parle ! Il me dit : ' + message);
-
+    socket.on('new_user',function(user){
+    	socket.user = user;
+    	socket.emit('welcome', 'Bienvenue à toi ' + user);
+    	console.log('New user ! ' + user);
     });
-    socket.on('cell', function (event) {
-        //console.log(event);
-        socket.emit('retour',event);
-    }); 
 
+    socket.on('placement_bateaux', function (event) {
+        //console.log(event);
+        console.log(bateaux);
+        bateaux.push(event);
+        socket.emit('retour_placement',bateaux);
+    }); 
+    socket.on('tir', function(event){
+    })
 });
 
 
