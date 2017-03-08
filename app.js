8ca9e01d979cff3dd86f4ac235e6f134a9c913d4
Cedric <cednewein@live.fr>
2017-03-08 19:30:48 +0100

    Première modifications de la page html et quelques tests jquery + node/socket

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 var http = require('http');
-var app = connect();
-var server = http.createServer(app); //cf app.js
+var jquery = require('jquery');
+var fs = require('fs');
 
-//start server
+
+// Chargement du fichier index.html affiché au client
+
+var server = http.createServer(function(req, res) {
+
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+
+        res.writeHead(200, {"Content-Type": "text/html"});
+
+        res.end(content);
+
+    });
+
+});
+
+
+// Chargement de socket.io
+
+var io = require('socket.io').listen(server);
+
+
+// Quand un client se connecte, on le note dans la console
+
+io.sockets.on('connection', function (socket) {
+
+    console.log('Un client est connecté !');
+    //socket.emit('message', 'Vous êtes bien connecté !');
+    socket.on('message', function (message) {
+
+        console.log('Un client me parle ! Il me dit : ' + message);
+
+    });
+    socket.on('cell', function (event) {
+        //console.log(event);
+        socket.emit('retour',event);
+    }); 
+
+});
+
+
 server.listen(8080);
-
-
-//get username
-function getUsername( socket, callback ) {
-  socket.get('username', function(err, username) {
-    if (! username) {
-      username = socket.id;
-    }
-    callback(err,username);
-  });
-}
-
-
-
-
